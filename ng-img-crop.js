8ab7d691ac98fb7ab5df8000b79c89234fe1628c
fferrara <thecrow.eric@gmail.com>
2015-11-12 11:42:15 -0200

    Error messages fixed

--- ng-img-crop.js
+++ ng-img-crop.js
@@ -995,16 +995,18 @@
                 temp_canvas = angular.element('<canvas></canvas>')[0];
                 temp_ctx = temp_canvas.getContext('2d');
                 var ris = this.getResultImageSize();
-                var adjusted_width = image.width *theArea.getSize().w/ctx.canvas.width;
-                var adjusted_height =  image.height*theArea.getSize().h/ctx.canvas.height;
-                temp_canvas.width =  adjusted_width;
-                temp_canvas.height = adjusted_height;
-                var center = theArea.getCenterPoint();
-                var retObj = {
-                    dataURI: null,
-                    imageData: null
-                };
-                if (image !== null) {
+                // TODO: pull request
+                if (image != null) {
+                    var adjusted_width = image.width *theArea.getSize().w/ctx.canvas.width;
+                    var adjusted_height =  image.height*theArea.getSize().h/ctx.canvas.height;
+                    temp_canvas.width =  adjusted_width;
+                    temp_canvas.height = adjusted_height;
+                    var center = theArea.getCenterPoint();
+                    var retObj = {
+                        dataURI: null,
+                        imageData: null
+                    };
+
 
                     console.log(temp_canvas);
                     console.log(ris);
@@ -1279,22 +1281,25 @@
 
                 var updateResultImage = function (scope) {
                     var resultImageObj = cropHost.getResultImage();
-                    var resultImage = resultImageObj.dataURI;
-                    if (storedResultImage !== resultImage) {
-                        storedResultImage = resultImage;
-                        if (angular.isDefined(scope.resultImage)) {
-                            scope.resultImage = resultImage;
-                        }
-                        if (angular.isDefined(scope.resultImageData)) {
-                            scope.resultImageData = resultImageObj.imageData;
-                        }
+                    if (resultImage) {
+                        var resultImage = resultImageObj.dataURI;
+                        if (storedResultImage !== resultImage) {
+                            storedResultImage = resultImage;
+                            if (angular.isDefined(scope.resultImage)) {
+                                scope.resultImage = resultImage;
+                            }
+                            if (angular.isDefined(scope.resultImageData)) {
+                                scope.resultImageData = resultImageObj.imageData;
+                            }
 
-                        updateAreaCoords(scope);
-                        scope.onChange({
-                            $dataURI: scope.resultImage,
-                            $imageData: scope.resultImageData
-                        });
+                            updateAreaCoords(scope);
+                            scope.onChange({
+                                $dataURI: scope.resultImage,
+                                $imageData: scope.resultImageData
+                            });
 
+
+                        }
                     }
                 };
 
